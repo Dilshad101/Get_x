1ac11c8df2cc0f4cb100a80a5c42d2d25c4fffc9
Dilshad <dilshadsanu17@gmail.com>
2023-08-11 20:18:49 +0530

    code updated

--- lib/controllers/student_controller.dart
+++ lib/controllers/student_controller.dart
@@ -4,13 +4,6 @@ import 'package:student_app/model/student_model.dart';
 class StudentController extends GetxController {
    var name='dd'.obs;
   final List<Rx<Student>> _studentList = [];
-  // addStudent(String name, int age, int batch, int regnum, int? id) {
-  //   final student =
-  //       Student(age: age, batch: batch, name: name, regnum: regnum).obs;
-  //   // Get.find<ListController>().addStudentToList(student);
-  //   _studentList.add(student);
-  // }
-
   addStd(Rx<Student> std) {
     _studentList.add(std);
     update();
